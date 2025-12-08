b844a7033784ee912e86fa4d616be391360ec2ed
Zahra <karimizahra369@gmail.com>
2025-12-08 12:48:46 +0100

    Add Sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'python-molecule_project'
+copyright = '2025, Zahra Karimi'
+author = 'Zahra Karimi'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',   # Reads docstrings from your code
+    'sphinx.ext.napoleon',  # Supports Google & NumPy style docstrings
+    'sphinx.ext.viewcode',  # Optional: adds links to the source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
